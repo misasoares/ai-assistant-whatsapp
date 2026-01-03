b482140743970228d7c8a0141019dccf8b930df9
misasoares <misasoaresms2@gmail.com>
2026-01-02 23:31:42 -0300

    chore(backend): configure global modules, routing prefix, and CORS

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,12 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EvolutionModule } from './evolution/evolution.module';
-
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [EvolutionModule, PrismaModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    EvolutionModule, 
+    PrismaModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
