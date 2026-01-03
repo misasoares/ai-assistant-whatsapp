ddbbc8eff1ab2e21159dc1d2f31f4c43c0f6d7aa
misasoares <misasoaresms2@gmail.com>
2026-01-02 23:31:38 -0300

    feat(backend): setup Prisma ORM and infrastructure dependencies

--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -1,13 +1,13 @@
-
 import { Injectable, OnModuleInit } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from '@prisma/client';
 import { Pool } from 'pg';
 import { PrismaPg } from '@prisma/adapter-pg';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
-  constructor() {
-    const connectionString = `${process.env.DATABASE_URL}`;
+  constructor(private configService: ConfigService) {
+    const connectionString = configService.get<string>('DATABASE_URL');
 
     const pool = new Pool({ connectionString });
     const adapter = new PrismaPg(pool);
